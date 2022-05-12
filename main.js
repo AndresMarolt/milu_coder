0f2d21d4517ba11334431de13dfaaeee850c4fbe
AndresMarolt <andresmmarolt@gmail.com>
2022-05-12 16:01:26 -0300

    filtro por categoria + retoques dropdown

--- main.js
+++ main.js
@@ -2,36 +2,33 @@ document.addEventListener('DOMContentLoaded', () => {
     obtenerProductos();
 });
 
-async function obtenerProductos() {
 
+async function obtenerProductos() {
     cargarLocalStorage();
 
     const resp = await fetch('../data.json');
     const articulosArray = await resp.json();
     
     const publicaciones = articulosArray.filter(art => {
-        const {categoria} = art; 
-        return categoria === localStorage.getItem('categoria');
+        const {categoria, tipo} = art;
+        return (categoria === localStorage.getItem('categoria') && tipo === localStorage.getItem('tipo'));
     });
 
+    console.log(publicaciones);
     imprimirProductos(publicaciones);
 }
 
 function cargarLocalStorage() {
-    const enlacesCategorias = document.querySelectorAll('.btn-categoria')
-    const enlacesTipos = document.querySelectorAll('.btn-tipo');
-
-    enlacesCategorias.forEach((enlace) => {
-        enlace.addEventListener("click", (e) => {
-            const categoria = e.target.innerText;
-            localStorage.setItem('categoria', categoria);
-        })
-    })
+    const enlacesTipos = document.querySelectorAll('.enlace-producto');
 
     enlacesTipos.forEach((enlace) => {
         enlace.addEventListener("click", (e) => {
             const tipo = e.target.innerText;
-            localStorage.setItem('tipo', tipo)
+            const categoria = e.target.parentElement.parentElement.previousElementSibling.innerText;
+            console.log(tipo);
+            console.log(categoria);
+            localStorage.setItem('tipo', tipo);
+            localStorage.setItem('categoria', categoria);
         })
     })
 }
@@ -39,19 +36,30 @@ function cargarLocalStorage() {
 function imprimirProductos(publicaciones) {
     const divPadre = document.getElementById('prods')
 
-    publicaciones.forEach((publicacion) => {
-        const nuevoElemento = document.createElement("DIV");
-        const {nombre, precio, imagen} = publicacion;
+    if(publicaciones.length > 0) {
+        publicaciones.forEach((publicacion) => {
+            const nuevoElemento = document.createElement("DIV");
+            const {nombre, precio, imagen} = publicacion;
+    
+            nuevoElemento.setAttribute("class", "producto");
+    
+            nuevoElemento.innerHTML = `
+                <img src=${imagen} alt="" class="w-100 img-producto">
+                <p class="mb-0">${nombre}</p>
+                <p>$${precio}</p>
+            `;
+            
+            divPadre.appendChild(nuevoElemento);
+        })
+    
+        console.log(publicaciones);
+    } else {
+        const nuevoElemento = document.createElement("H1");
+        nuevoElemento.setAttribute("style", "grid-column: 1 / 5")
 
-        nuevoElemento.setAttribute("class", "producto");
+        nuevoElemento.innerText = "No se encontraron resultados en esta categoría."
 
-        nuevoElemento.innerHTML = `
-            <img src=${imagen} alt="" class="w-100 img-producto">
-            <p class="mb-0">${nombre}</p>
-            <p>$${precio}</p>
-        `;
-        
-        divPadre.appendChild(nuevoElemento);
-    })
+        divPadre.appendChild(nuevoElemento)
+    }
 
 }
